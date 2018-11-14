ed410288449eeb057e676e075b264082fed8ff54
Will Anderson <will@W-Anderson-MacBook-Pro.local>
2018-11-14 15:14:53 -0600

    removed custom favicon options in favor of built-in Customizer > Site Identity > Site Icon

--- header.php
+++ header.php
@@ -6,9 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-	
-	<?php get_template_part('components/favicon'); ?>
-	
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_head(); ?>
