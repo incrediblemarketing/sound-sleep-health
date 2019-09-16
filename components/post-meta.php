777a148fa759623915e53848a79149f88433f200
Will Anderson <will@incrediblemarketing.com>
2019-09-16 13:48:08 -0500

    simplified post-meta

--- components/post-meta.php
+++ components/post-meta.php
@@ -1,17 +1,12 @@
-<section class="post-meta">
-	<small>
-		<span><i class="fa fa-calendar"></i> <?php echo get_the_date(); ?></span>
-		<span><i class="fa fa-user"></i> <?php echo get_the_author(); ?></span>
-		<span><i class="fa fa-folder-open"></i>
-			<?php
-				$categories = get_the_category();
-				$output = array();
-				foreach ($categories as $category) { 
-					$category_link = get_category_link($category->ID);
-					$output[] = '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( $category->name ) . '</a>';
-				}
-				echo implode(', ', $output);
-			?>
-		</span>
-	</small>
-</section>
+<div class="post-meta">
+	<?php echo get_the_date(); ?> by <?php echo get_the_author(); ?> in 
+	<?php
+		$categories = get_the_category();
+		$output = array();
+		foreach ($categories as $category) { 
+			$category_link = get_category_link($category->ID);
+			$output[] = '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( $category->name ) . '</a>';
+		}
+		echo implode(', ', $output);
+	?>
+</div>
